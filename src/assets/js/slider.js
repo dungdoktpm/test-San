1f2103491cf890c14ba9735e3c35cb35e5dbe1b6
DungDoktpm <dungdoktpm@gmail.com>
2020-10-10 12:24:59 +0700

    fix home page

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,5 +1,23 @@
 console.log('slide');
-var swiper = new Swiper('.home-slider .swiper-container', {
+var swiper01 = new Swiper('.home-slider .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper02 = new Swiper('.pricing .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper03 = new Swiper('.testimonials .swiper-container', {
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
